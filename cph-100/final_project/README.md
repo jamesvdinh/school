b7856a1155531ff7227b04693fe53f761228f0d5
James Dinh <jamesdinh77@gmail.com>
2025-12-08 19:15:32 -0800

    added data notebooks

---
+++ cph-100/final_project/README.md
@@ -0,0 +1,5 @@
+# Diabetes Phenotyping Using MIMIC-IV ICU Data and Logistic Regression
+
+> CPH 100 Final Project
+>
+> Author: James Dinh
---
+++ cph-100/final_project/src/utils/data_utils.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Diabetes ICD Codes
+DIABETES_CODES = ['250', 'E10', 'E11', 'E13']
+
+
+def load_data(file_path):
+    """Load diagnoses data from a CSV file."""
+    return pd.read_csv(file_path, compression='gzip', low_memory=False)
+
+
+def get_diabetic_patients(diagnoses_df):
+    """Extract diagnoses related to diabetes."""
+    diabetes_diagnoses = diagnoses_df[diagnoses_df['icd_code'].str.startswith(
+        tuple(DIABETES_CODES))]
+    return diabetes_diagnoses
+
+
+def filter_to_cohort(df, subject_ids):
+    return df[df['subject_id'].isin(subject_ids)]

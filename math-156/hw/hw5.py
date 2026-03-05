3a8f6af2436709d3c4571671a3a3cc576fc62f95
James Dinh <jamesdinh77@gmail.com>
2026-03-05 10:57:40 -0800

    hw5

--- math-156/hw/hw5.py
+++ math-156/hw/hw5.py
@@ -1,4 +1,21 @@
 import numpy as np
 
 
-def solve_lu(A_in, b_in):
+def compute_lu_product(L, U):
+    n = L.shape[0]
+    A = np.zeros((n, n))
+
+    for i in range(n):
+        for j in range(n):
+            limit = min(i, j) + 1
+            A[i, j] = np.dot(L[i, :limit], U[:limit, j])
+
+    return A
+
+
+L = np.array([[1, 0, 0], [0.5, 1, 0], [0.25, 0.75, 1]])
+U = np.array([[4, 2, -1], [0, 1, 0.5], [0, 0, 2]])
+print(f"L:\n{L}\n")
+print(f"U:\n{U}\n")
+A = compute_lu_product(L, U)
+print(f"A:\n{A}")
